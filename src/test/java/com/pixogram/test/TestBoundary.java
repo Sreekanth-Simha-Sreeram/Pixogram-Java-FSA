723939b9445cabe0337d3521b698d76797b16910
Jesuraj77 <isysraju2001@gmail.com>
2020-02-28 14:54:24 +0530

    Added

--- src/test/java/com/pixogram/test/TestBoundary.java
+++ src/test/java/com/pixogram/test/TestBoundary.java
@@ -3,7 +3,6 @@ package com.pixogram.test;
 import static org.junit.Assert.assertEquals;
 
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
